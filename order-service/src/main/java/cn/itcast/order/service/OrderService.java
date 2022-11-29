f59e895205d5dbcdfd88ab60a75dea2aafb144fb
zhaolixin <jason.zhao@xiding.com.cn>
2022-11-29 09:43:01 +0800

    first commit

---
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -0,0 +1,27 @@
+package cn.itcast.order.service;
+
+import cn.itcast.order.mapper.OrderMapper;
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+    @Autowired
+    private RestTemplate restTemplate;
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        System.out.println("1122233");
+        String url="http://userService/user/"+order.getUserId();
+        User forObject = restTemplate.getForObject(url, User.class);
+        order.setUser(forObject);
+        // 4.返回
+        return order;
+    }
+}
